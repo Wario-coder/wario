9f2cff32d76bb6f67713319892345598dd27c292
wario-coder <warioboru22@gmail.com>
2024-10-17 15:43:53 +0300

    initial commit

---
+++ Assests/js/main.js.ts
@@ -0,0 +1,6 @@
+const javaclass:HTMLElement=document.getElementById(elementid:"colorbutton");
+// add event listener for the button click
+javaclass.addEventListener("click",listner():void=>{
+
+    javaclass.style.backgroundColor=javaclass.style.backgroundColor=="red" ? #4CAF50:"red"
+});
